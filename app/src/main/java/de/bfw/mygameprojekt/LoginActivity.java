04d16d09c234535bc9ce801bcafa6185367a7779
Brian <Otto72886@m365.bfwbb-lernen.de>
2025-03-31 09:13:06 +0200

    Hide signupbutton if user has already signed up

--- app/src/main/java/de/bfw/mygameprojekt/LoginActivity.java
+++ app/src/main/java/de/bfw/mygameprojekt/LoginActivity.java
@@ -1,14 +1,23 @@
 package de.bfw.mygameprojekt;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+
+    AppCompatButton switchToSignupButton;
+
+    SharedPreferences sharedPreferences;
+    boolean hasSignedUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,14 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        sharedPreferences = getSharedPreferences("userPrefs", Context.MODE_PRIVATE);
+        hasSignedUp = sharedPreferences.getBoolean("hasSignedUp", false);
+
+        switchToSignupButton = findViewById(R.id.switchToSignupButton);
+
+        if (hasSignedUp) {
+            switchToSignupButton.setVisibility(View.GONE);
+        }
     }
 }
