fe1219ed143b7d2dba2cd9b06477d52a78f60af0
Brian <Otto72886@m365.bfwbb-lernen.de>
2025-03-31 09:45:59 +0200

    Add info if user wants to submit empty fields

--- app/src/main/java/de/bfw/mygameprojekt/RegisterActivity.java
+++ app/src/main/java/de/bfw/mygameprojekt/RegisterActivity.java
@@ -1,14 +1,26 @@
 package de.bfw.mygameprojekt;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText usernameInput;
+    EditText passwordInput;
+    EditText confirmPasswordInput;
+    AppCompatButton submitButton;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,27 @@ public class RegisterActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        usernameInput = findViewById(R.id.usernameInput);
+        passwordInput = findViewById(R.id.passwordInput);
+        confirmPasswordInput = findViewById(R.id.confirmPasswordInput);
+        submitButton = findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(this);
+        editor = getSharedPreferences("userPrefs", Context.MODE_PRIVATE).edit();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == submitButton.getId()) {
+            String username = usernameInput.getText().toString();
+            String password = passwordInput.getText().toString();
+            String confirmPassword = confirmPasswordInput.getText().toString();
+
+            if (username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+                Toast.makeText(this, "Alle Felder ausfüllen", Toast.LENGTH_SHORT).show();
+            }
+        }
+
     }
 }
