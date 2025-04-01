f8e38262a1998acb74d2e0662880874659be0b36
Brian <Otto72886@m365.bfwbb-lernen.de>
2025-04-01 08:33:57 +0200

    Add switching between light- and darkmode

--- app/src/main/java/de/bfw/mygameprojekt/SettingsActivity.java
+++ app/src/main/java/de/bfw/mygameprojekt/SettingsActivity.java
@@ -1,19 +1,25 @@
 package de.bfw.mygameprojekt;
 
+import android.app.UiModeManager;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView usernameText;
+    AppCompatButton lightModeButton;
+    AppCompatButton darkModeButton;
 
     SharedPreferences sharedPreferences;
 
@@ -30,7 +36,21 @@ public class SettingsActivity extends AppCompatActivity {
 
         usernameText = findViewById(R.id.usernameText);
         sharedPreferences = getSharedPreferences("userPrefs", Context.MODE_PRIVATE);
+        lightModeButton = findViewById(R.id.lightModeButton);
+        darkModeButton = findViewById(R.id.darkModeButton);
 
         usernameText.setText(sharedPreferences.getString("username", ""));
+        lightModeButton.setOnClickListener(this);
+        darkModeButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == lightModeButton.getId()) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+        if (v.getId() == darkModeButton.getId()) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
     }
 }
